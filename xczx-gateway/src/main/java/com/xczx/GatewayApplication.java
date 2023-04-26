fa4ead1defdb8f0f1da1b9b7e97526425eecff39
ilovesshan <2665939276@qq.com>
2023-04-26 16:57:40 +0800

    feat: add gateway

---
+++ xczx-gateway/src/main/java/com/xczx/GatewayApplication.java
@@ -0,0 +1,19 @@
+package com.xczx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author: ilovesshan
+ * @date: 2023/4/26
+ * @description:
+ */
+
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
