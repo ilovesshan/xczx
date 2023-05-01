195b7abf585440a9fb6a957c1f13687eb94227fc
ilovesshan <2665939276@qq.com>
2023-05-01 08:26:13 +0800

    feat: add message sdk

---
+++ xczx-message-sdk/src/main/java/com/xczx/messagesdk/service/MqMessageHistoryService.java
@@ -0,0 +1,16 @@
+package com.xczx.messagesdk.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.xczx.messagesdk.model.po.MqMessageHistory;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author itcast
+ * @since 2022-09-21
+ */
+public interface MqMessageHistoryService extends IService<MqMessageHistory> {
+
+}
