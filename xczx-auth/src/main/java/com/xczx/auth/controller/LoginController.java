7356c36d177b57ca464dbfa4f2380a8a44efead7
ilovesshan <2665939276@qq.com>
2023-05-02 09:24:08 +0800

    docs : update document comment

--- xczx-auth/src/main/java/com/xczx/auth/controller/LoginController.java
+++ xczx-auth/src/main/java/com/xczx/auth/controller/LoginController.java
@@ -3,18 +3,14 @@ package com.xczx.auth.controller;
 import com.xczx.ucenter.mapper.XcUserMapper;
 import com.xczx.ucenter.model.po.XcUser;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
-/**
- * @author Mr.M
- * @version 1.0
- * @description 测试controller
- * @date 2022/9/27 17:25
- */
+
 @Slf4j
 @RestController
 public class LoginController {
@@ -36,14 +32,15 @@ public class LoginController {
     }
 
     @RequestMapping("/r/r1")
+    @PreAuthorize("hasAuthority('p1')")
     public String r1() {
         return "访问r1资源";
     }
 
     @RequestMapping("/r/r2")
+    @PreAuthorize("hasAuthority('p2')")
     public String r2() {
         return "访问r2资源";
     }
-
 
 }
