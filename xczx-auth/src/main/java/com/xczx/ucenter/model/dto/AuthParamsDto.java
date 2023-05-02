97b247a8280d4df474f4381733f4ce99121b198e
ilovesshan <2665939276@qq.com>
2023-05-02 09:23:59 +0800

    docs : update document comment

--- xczx-auth/src/main/java/com/xczx/ucenter/model/dto/AuthParamsDto.java
+++ xczx-auth/src/main/java/com/xczx/ucenter/model/dto/AuthParamsDto.java
@@ -5,12 +5,7 @@ import lombok.Data;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * @author Mr.M
- * @version 1.0
- * @description 认证用户请求参数
- * @date 2022/9/29 10:56
- */
+
 @Data
 public class AuthParamsDto {
 
